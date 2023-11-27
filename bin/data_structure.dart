ee7913ed32e6d54634bac9e26ba4129d7322fa9e
Kamran Khan <kamran.khan@rnssol.com>
2023-11-27 21:57:32 +0500

    Initial Commit

---
+++ bin/data_structure.dart
@@ -0,0 +1,16 @@
+
+
+import 'dart:math';
+
+import 'package:data_structure/linked_list/single_linkedlist.dart';
+
+void main(List<String> arguments) {
+  var linkedList  = SingleLinkedList();
+  linkedList.generateSingleLinked(noOfNodes:5);
+  linkedList.traverseSingleLinked();
+  print('\n');
+//   linkedList.addNodeEnd(data: Random().nextInt(100));
+//   linkedList.addNodeStart(data: Random().nextInt(100));
+  linkedList.addNodeAt(position:3, data: Random().nextInt(100));
+  linkedList.traverseSingleLinked();
+}
